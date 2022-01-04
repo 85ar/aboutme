ae76fc804d6964f21535698fdfd6c0c91e1bd386
85ar <85ar@rambler.ru>
2022-01-04 13:40:15 +0500

    base structure

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import AppContent from "components/AppContent";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <AppContent />
+    </div>
+  );
+}
+
+export default App;
